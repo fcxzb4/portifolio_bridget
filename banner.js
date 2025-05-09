344149750966a57769cd4c96e933a4c54d9beddc
Bridget <84099228+fcxzb4@users.noreply.github.com>
2025-05-09 15:50:19 -0300

    fa

--- banner.js
+++ banner.js
@@ -1,4 +1,24 @@
-export function switchBanner (n_bannner){
-         let banners = document.getElementById('banners')
-         banners.style.left = ;
-};
+let actual_banner = 1;
+const qnt_banners = 3;
+
+export function switchBanner(n_banner){
+    let banners = document.getElementById('banners');
+    banners.style.left = `-${n_banner - 1}00vw`;
+    actual_banner = n_banner;
+
+    for(let i = 1; i < qnt_banners + 1; i++){
+        let btn = document.getElementById(`btn_banner_${i}`);
+        if(n_banner == i){
+            btn.style.transform = 'scale(1.2)';
+        }else{
+            btn.style.transform = 'scale(1)'
+        }
+    }
+}
+
+setInterval(()=>{
+    if(actual_banner >= qnt_banners){
+        actual_banner = 0;
+    }
+    switchBanner(actual_banner + 1);
+}, 4000);
